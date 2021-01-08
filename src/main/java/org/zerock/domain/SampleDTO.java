a2faff49ae262a9952a90427d7918acdcc740d68
harbor_@naver.com <harbor_@naver.com>
2021-01-08 17:00:48 +0900

    20210108

---
+++ src/main/java/org/zerock/domain/SampleDTO.java
@@ -0,0 +1,15 @@
+package org.zerock.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class SampleDTO {
+	
+	private String name;
+	private int age;
+
+}
